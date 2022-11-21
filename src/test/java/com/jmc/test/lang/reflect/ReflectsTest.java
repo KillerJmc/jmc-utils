dc0de98cb1ed4314b4a1cc529ab5b1142fe0729e
Jmc <1934497212@qq.com>
2022-11-22 01:51:41 +0800

    Add getClassPath, listResources method to Reflects.

--- src/test/java/com/jmc/test/lang/reflect/ReflectsTest.java
+++ src/test/java/com/jmc/test/lang/reflect/ReflectsTest.java
@@ -1,5 +1,6 @@
 package com.jmc.test.lang.reflect;
 
+import com.jmc.lang.Outs;
 import com.jmc.lang.Strs;
 import com.jmc.lang.Tries;
 import com.jmc.lang.reflect.Reflects;
@@ -38,5 +39,33 @@ public class ReflectsTest {
     public void illegalAccessTest() {
         Tries.tryHandlesE(() -> Reflects.getStaticMethod(Strs.class, "isNum"),
                 System.err::println);
+    }
+
+    @Test
+    public void classPathTest() {
+        // 获取非在jar内的类路径
+        Reflects.getClassPath(this.getClass()).ifPresentOrElse(
+                System.out::println,
+                () -> { throw new RuntimeException("找不到类路径"); }
+        );
+
+        // 获取在jar内的类路径
+        Reflects.getClassPath(Test.class).ifPresentOrElse(
+                System.out::println,
+                () -> { throw new RuntimeException("找不到类路径"); }
+        );
+    }
+
+    @Test
+    public void listResourceTest() {
+        // 普通类路径（非jar路径）
+        Reflects.listResources(ReflectsTest.class, "/com/jmc/test")
+                .forEach(System.out::println);
+
+        Outs.newLine();
+
+        // jar路径
+        Reflects.listResources(Test.class, "/org/junit")
+                .forEach(System.out::println);
     }
 }
